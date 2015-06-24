3612ee1c12f7ed5943099ae1fa979b7a8a361a75
Tony Xu <tonylixu@gmail.com>
2015-06-24 11:56:50 -0400

    Import run from fabric

--- cassandra_snapshotter/snapshotting.py
+++ cassandra_snapshotter/snapshotting.py
@@ -4,7 +4,7 @@ from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 from boto.exception import S3ResponseError
 from datetime import datetime
-from fabric.api import (env, execute, hide, sudo)
+from fabric.api import (env, execute, hide, run, sudo)
 from fabric.context_managers import settings
 from multiprocessing.dummy import Pool
 import json
